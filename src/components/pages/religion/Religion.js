eda4006e46ab694134a7301af81a5c967857402d
Michael Fahnlander <michael.fahnlander@trojans.dsu.edu>
2023-04-05 13:50:00 -0500

    format

--- src/components/pages/religion/Religion.js
+++ src/components/pages/religion/Religion.js
@@ -5,27 +5,27 @@ function Religion() {
     return (
 <div class="mt-5 m-auto p-5 w-4/5 text-dark bg-bg-gray rounded-lg dark:text-bg-gray dark:bg-navy">
     <div class="lg:w-4/5 sm:w-7/8 mr-auto ml-auto">
-    <div class="ring-dark p-2 ml-1 mr-1">
-        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="https://jesusisreal-stl.org/">Evidence of the Existence of Jesus Christ</a>
+    <div class="ring-dark p-2">
+        <div class=" "><a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold ml-auto mr-auto text-center dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="https://jesusisreal-stl.org/">Evidence of the Existence of Jesus Christ</a></div>
         <div class="text-base pt-5">
             This website details some of the miracles directly related to the existence of Jesus Christ such as the Shroud of Turin, the Veil of Manoppello, and the Veil of Veronica. It also details a miracle regarding the Holy Eucharist turning into heart tissue with a distinct AB blood type. This heart tissue can still be observed in a church in Luciano, Italy where it has remained incorrupt without the assistance of preservers.
         </div>
     </div>
 
-    <div class="ring-dark p-2 ml-1 mr-1">
-        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.miracolieucaristici.org/en/Liste/list.html">Index of Eucharistic Miracles</a>
+    <div class="ring-dark p-2">
+        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center ml-auto mr-auto dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.miracolieucaristici.org/en/Liste/list.html">Index of Eucharistic Miracles</a>
         <div class="text-base pt-5">
             This is the most comprehensive list of Eucharistic Miracles ever, compiled by Blessed Carlos Acutis. It details miracles from all over the world, many of which have been thoroughly investigated by doctors and medical experts. In every instance, the bread blessed by a Catholic priest has turned into heart tissue that doctors say underwent intense stress consistent with the horrific death Jesus died. The circumstances surrounding these events make the possibility that the miracle was faked completely impossible. Many of the samples of heart tissue and blood are incorrupt and can still be viewed to this day.
         </div>
     </div>
-    <div class="ring-dark p-2 ml-1 mr-1 ">
-        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.carloacutis.com/en/association/presentazione">Blessed Carlos Acutis</a>
+    <div class="ring-dark p-2 ">
+        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center ml-auto mr-auto dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.carloacutis.com/en/association/presentazione">Blessed Carlos Acutis</a>
         <div class="text-base pt-5">
             This link tells the story of the life of Blessed Carlos Acutis, a 21st century web developer who battled cancer as a teenager but used his talents to serve God by bringing centuries of miracles into a globally accessible medium. He has been declared Blessed by the Catholic Church, which is the last step before being declared a saint.
         </div>
     </div>
-    <div class="ring-dark p-2 ml-1 mr-1">
-        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.therealpresence.org/">Catholic Resource</a>
+    <div class="ring-dark p-2">
+        <a class="hover:ring-lowTeal hover:text-lowTeal text-lg font-bold text-center ml-auto mr-auto dark:ring dark:ring-lowTeal dark:hover:ring-midTeal dark:hover:text-teal" href="http://www.therealpresence.org/">Catholic Resource</a>
         <div class="text-base pt-5">
             This website contains many resources such as the Biblical backing for the Holy Eucharist, angelic apparitions, and fundemental truths of the Catholic Church. Some of these apparitions even occurred in the 20th Century with tens of thousands of eye witnesses like in the case of the Miracle of Fatima.
         </div>
