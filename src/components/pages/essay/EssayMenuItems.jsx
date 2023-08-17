c529d929c0fb9aae2456ae4c5ceaabbee10536d5
Michael Fahnlander <michael.fahnlander@trojans.dsu.edu>
2023-08-17 13:01:43 -0500

    add sitemap

--- src/components/pages/essay/EssayMenuItems.jsx
+++ src/components/pages/essay/EssayMenuItems.jsx
@@ -5,11 +5,6 @@ export const EssayMenuItems = [
         cName: 'essay-dropdown-link'
     },
     {
-        title: 'Stopping Porn',
-        path: '/essays/Stopping-Porn',
-        cName: 'essay-dropdown-link'
-    },
-    {
         title: 'Survey of the Role of Generative Language Models in Education',
         path: '/essays/Generative-Language-Models-In-Education',
         cName: 'essay-dropdown-link'
--- src/components/pages/essay/PornsEffectOnRelationships.jsx
+++ src/components/pages/essay/PornsEffectOnRelationships.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, Link} from 'react';
 
 function Porn(){
     return(
@@ -10,51 +10,54 @@ function Porn(){
         You probably clicked on this link out of a devious curiosity, so I am sorry to disappoint. The nature of this article is entirely more serious. Having been raised in a fairly religious household, I was told that pornography was evil and that, contrary to my basic human desires, must refrain from partaking in it. This led to substantial internal dissonance as I was caught between my religious upbringing and the powerful desires that pornography so easily instigates. In this struggle, I decided to begin my own research and figure out if there is more to the story than unlimited pleasure and strict abstinence. I have made an incredible number of discoveries that sparked me to begin compiling all the research I could find and create a medium with which to share this knowledge with others. The fruition of this website is something I am quite proud of since this whole project has been over five years in the making. I was going to analyze relationships through the lens of internet pornography as my senior thesis for my undergraduate degree, although I was advised that I did not have enough time to finish it. Despite this setback, I chose to continue working on it and have concluded that pornography, through its physical, social, and mental harms, really is the toxic poison for true, meaningful relationships, to say nothing of its spiritual harms. Make no mistake, however, that this analysis includes religious or other biases as I have taken great care to only focus on published research.</p>
 
         <h1 class="text-lg text-center text-darkdark:text-bg-gray font-bold">Introduction</h1>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         In the year 2019, the world uploaded 169 years' worth of pornographic videos onto one website. For perspective, former U.S. President Millard Fillmore would still be watching the content that was uploaded to one website in one year if he had started watching it during his presidency in 1852 [1]. The stunning quantity of pornography created and consumed warrants an analysis of how this media attracts such popularity and, since it popularizes the most intimate parts of relationships, if it adversely effects other facets of life, specifically real-life relationships. A thorough analysis of the available literature reveals that pornography addicts consumers by offering an easy, idealized alternative to personal intimacy which undermines real relationships and causes separation and divorce among couples.
         </p>
 
         <h1 class="text-lg text-center font-bold">Historical Pornography Usage</h1>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         The world watches a significant amount of pornography, or Sexually Explicit Material as it is referred to in the literature, every year; so much so that the largest SEM websites in the world must break down their internet traffic statistics by minutes in order for normal people to comprehend and appreciate them. Tube sites like, where users upload their own content, garner an astonishing number of visits. Pornhub, the most popular tube site in the world, rakes in over 90 million visits per day. At this rate, it would take just over two months for every person on Earth to visit the site [2]. Similarly, visitors to the site consume an astronomical amount of content. Using Pornhub's 2019 data, the website reports that visitors to the website watched 664,920 hours of SEM every hour. That means over 11,000 hours of SEM videos are watched every 60 seconds. Furthermore, visitors watched over 13 million videos every hour, meaning 216,985 videos every minute [1]. The United States, according to Pornhub's 2019 data, far and away consumes the most content on the site [3]. Internet research organization WebRoot calculates that 40 million Americans regularly visit SEM sites [4]. The incredible amount of time that people spend watching pornography and growing concerns has prompted research into the multi-billion-dollar industry to study the effects on people and relationships which will be discussed.
         </p>
 
         <h1 class="text-lg text-center font-bold">Literature Review</h1>
         <h2 class="font-bold text-center">Introduction</h2>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         One of the reasons that Sexually Explicit Material (SEM) has grown so popular stems from its powerful capitalization of human neuroplasticity and neurochemistry to form addictions. Traditional addiction theory must be understood if SEM's seemingly unbreaking grip on the mind of humanity is to be understood. The most powerful stimuli satisfy basic human needs such as eating, engaging in social environments, sex, and other fundamental needs in Maslow's Hierarchy of needs. These activities trigger the release of the most powerful chemicals in the brain like dopamine which is triggered when people engage in activities proven useful in evolution [5].
         </p>
 
         <h2 class="font-bold text-center">Addiction Model</h2>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         Sexually Explicit Material causes the release of powerful neurochemicals that human evolution has built up over centuries. Maslow categorized the needs of people into a pyramid with the most basic needs at the bottom such as food, water, shelter, and reproduction. SEM taps into the most basic need of reproduction by simulating and stimulating those neural pathways [6]. When a viewer first encounters pornography, they experience what researchers call the “Binge/Intoxication Stage” where addiction first begins because the stimulus is associated with the dopamine rush [7]. After experiencing SEM, the viewer enters the “Withdrawal/Negative Affect Stage”. After the initial rush, other smaller stimuli that might otherwise have elicited a dopamine response, albeit a smaller one than the addictive stimulus, do not. Dopamine receptors in the brain have become “saturated” from the rush of dopamine due to a chemical called Dynorphin. The release of Dynorphin decreases receptivity to neurochemicals like Dopamine but simultaneously increases sensitivity to that stimulus. This effect can lead to lasting tolerance and to feelings of anxiety, depression, dysphoria, and irritability [8], [7], [9]. The third state of addition, “Preoccupation/Anticipation Stage”, occurs when the dopamine response is temporarily compromised. In this stage of the cycle, a brain's neuroplasticity has created or reinforced pathways other areas of the brain, and it has become dependent on the stimulus to maintain baseline levels of dopamine. This effect manifests itself as a yearning or craving feeling to achieve the high that it previously experienced [10], [8]. Love, et al. find SEM addiction consistent with this model originally conceived with only drug addiction in mind [8]. Scientific theories about the human brain have evolved over time just like any scientific structure, and scientists have broken human desires into two different categories: liking and wanting. Liking means a fundamental and biological attraction towards specific activities like hobbies or pastimes. Conversely, people learn how to want based on the neurochemical reactions of the human brain to stimuli, or broadly pursuing things that result in dopamine release. Examples of wanting include drug dependencies and addiction [11].
         </p>
 
         <h2 class="font-bold text-center">Addictive Properties of SEM</h2>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         In one FMRI study of 57 heterosexual males ages 18-48, 31 seeking treatment for Problematic Pornography Usage (PPM) and 26 control subjects were given a series of tests. Men seeking treatment for Problematic Pornography Usage exhibited more activity in the ventral striatum (the pleasure center of the brain) during the anticipatory phase of the test than the control group. Exposure to SEM was the reward for answering questions correctly, meaning they had stronger motivations for getting the question correct. Subjects reporting PPU showed higher sensitivity to Sexually Explicit Material than control group, showing that PPU alters the motivational process. Gola, et al. also found that strength of fMRI results significantly correlated to frequency of pornography use and masturbation [12].
         </p>
 
         <h2 class="font-bold text-center">Violent Tendancies</h2>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         An analysis of 4009 scenes from the top categories of two tube sites reveals disturbing statistics regarding the treatment of porn actresses. Almost half (45%) of pornographic videos depict physical aggression towards women who make up 97% of the recipients of the violence and who almost exclusively react in neutral or positive manners [15]. The agents who coordinate porn actors and actresses and porn film companies intentionally withhold from the actors or actresses what a porn scene will entail until after extensive preparation for the scene has been completed, such as hair and makeup, to reduce the likelihood of an actor or actress turning down a role [16]. Thus, actors and actresses undergo manipulation to carry out violent sex acts that normal spouses or partners will not. Married men or male partners who view pornography grow accustomed to encountering this violent behavior in intimacy due to pornography's escalating properties. Additional studies show that the violent behaviors are not enjoyed by the spouses or partners of porn viewers who find them “objectionable or demeaning” to the point of filing for divorce [17]. 
         </p>
 
         <h1 class="text-lg font-bold text-center">Porn's Effect on Relationship</h1>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         A 2019 analysis of 30 nationally representative surveys found pornography to be associated with worse relationship quality for both men and women in both married and unmarried relationships. This study carries special weight because it controlled for other factors that scholars had previously found problematic when drawing conclusions from datasets. While this study shies away from claiming a causal link between pornography and lower quality relationships, it offers a basis for other studies [17].
         </p>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         Viewing pornography more than once per month is correlated with decreased relationship satisfaction and dissatisfaction increases with frequency of pornography use. [10].
         </p>
 
         <h2 class="font-bold text-center">Divorce</h2>
-        <p>
+        <p class="text-base text-dark dark:text-bg-gray">
         According to Doran and Price, watching pornography made married adults more than twice as likely to be divorced in six years, cheat on their spouse, and report less happiness in their marriage than control groups who did not view pornography [18].
         </p>
 
-        https://truthaboutporn.org/
+        <Link class='text-lowTeal hover:text-midTeal text-lg dark:hover:text-midTeal' to='https://truthaboutporn.org/'>Truth About Porn</Link>
 
+        <p class="text-base text-dark dark:text-bg-gray">
+            If you found this essay pursuasive and are interested in learning more, you can read my subsequent article that has other media I have encountered that is easier to digest than highly scientific articles. There are YouTube videos, articles, and analyses that have also informed my opinions on this topic. It also contains my personal experience with cutting porn out of my life and tips I have since it is a challenging endeavor. <Link class='text-lowTeal hover:text-midTeal text-md dark:hover:text-midTeal' to='/essays/Stopping-Porn'>Porn Article 2</Link>.
+        </p>
 
         <h1 class="font-bold text-lg text-center">References</h1>
 <p>[1] 	Pornhub, "The 2019 Year in Review," 11 December </p>2019. [Online]. Available: https://www.pornhub.com/insights/2019-year-in-review#2019.
